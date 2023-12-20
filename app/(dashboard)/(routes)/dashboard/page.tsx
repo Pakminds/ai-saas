932a47f17331cbb947511b40e0fbeadfe5a7f60c
Wajid Ali <76617505+wajid996@users.noreply.github.com>
2023-12-20 14:46:48 +0500

    ww

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -3,11 +3,8 @@ import { UserButton } from "@clerk/nextjs";
 export default function Dashboard() {
     return (
         <>
-            <div className="container  py-4 flex justify-between items-center">
-                <div><h4 className="font-bold text-lg">Dashboard</h4></div>
-                <div>
-                    <UserButton afterSignOutUrl="/" />
-                </div>
+            <div className="container">
+                <h4 className="font-bold text-lg">Dashboard</h4>
             </div>
         </>
     );
